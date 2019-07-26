450d7f77c83f10811732ac1cb98e092ee8e3ddf3
Evan Mulawski <evan_mulawski@uml.edu>
2019-07-26 12:37:15 -0400

    initial commit

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [Route("api/all")]
+        public IDictionary<string, string> GetAll()
+        {
+            var config = new Dictionary<string, string>();
+
+            foreach (var appSetting in ConfigurationManager.AppSettings.AllKeys)
+                config[$"AS_{appSetting}"] = ConfigurationManager.AppSettings[appSetting];
+
+            foreach (ConnectionStringSettings connStr in ConfigurationManager.ConnectionStrings)
+                config[$"CS_{connStr.Name}"] = connStr.ConnectionString;
+
+            return config;
+        }
+
+        [Route("api/appsettings")]
+        public IDictionary<string, string> GetAppSettings()
+        {
+            var config = new Dictionary<string, string>();
+
+            foreach (var appSetting in ConfigurationManager.AppSettings.AllKeys)
+                config[appSetting] = ConfigurationManager.AppSettings[appSetting];
+
+            return config;
+        }
+
+        [Route("api/connectionstrings")]
+        public IDictionary<string, string> GetConnectionStrings()
+        {
+            var config = new Dictionary<string, string>();
+
+            foreach (ConnectionStringSettings connStr in ConfigurationManager.ConnectionStrings)
+                config[connStr.Name] = connStr.ConnectionString;
+
+            return config;
+        }
+    }
+}
